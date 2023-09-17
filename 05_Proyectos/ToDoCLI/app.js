b2d829daf19daae2a0da758d4b167684710ef13e
fcegatti <fcegatti@yahoo.es>
2023-09-18 00:29:56 +0200

    Create app with chalk and createInterface from readline

---
+++ 05_Proyectos/ToDoCLI/app.js
@@ -0,0 +1,19 @@
+import { createInterface } from 'readline';
+import chalk from 'chalk';
+
+const tasks = [];
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function displayMenu() {
+  console.log(`🦊🦊🦊🦊🦊${chalk.redBright.bold('To Do App')}🦊🦊🦊🦊🦊`);
+  console.log('1. Agregar tarea');
+  console.log('2. Listar tareas');
+  console.log('3. Completar tarea');
+  console.log('4. Salir');
+}
+
+displayMenu();
