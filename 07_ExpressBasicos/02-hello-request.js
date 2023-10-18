f9cbb1bbe8a8d4f11ba852c40daf97522d0f63c6
fcegatti <fcegatti@yahoo.es>
2023-10-18 02:55:43 +0200

    req.query in Express

--- 07_ExpressBasicos/02-hello-request.js
+++ 07_ExpressBasicos/02-hello-request.js
@@ -14,9 +14,14 @@ app.get('/user/:id-:name-:age', (req, res) => {
     </h1>`
   );
 });
-
-app.get('/', (req, res) => {
-  res.end('<h1>Hola mundo desde Express.js</h1>');
+// http://localhost:3000/search?id=19&name=Jon&age=40
+app.get('/search', (req, res) => {
+  res.set({"Content-Type": "text/html; charset=utf-8" });
+  res.end(
+    `<h1>
+      ${req.query.name}, bienvenidos a Express.js- Tu id es ${req.query.id} y tienes ${req.query.age} años.
+    </h1>`
+  );
 });
 
 
