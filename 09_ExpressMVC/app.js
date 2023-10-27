ed475f32408b1df1f7f2ea4ee9a10a2b70932fa3
fcegatti <fcegatti@yahoo.es>
2023-10-27 19:10:47 +0200

    Set up routes and controllers

--- 09_ExpressMVC/app.js
+++ 09_ExpressMVC/app.js
@@ -3,7 +3,8 @@ import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-// import taskController from '.controllers/taskController.js';
+import taskController from './controllers/taskController.js';
+import errorController from './controllers/errorController.js';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
@@ -16,6 +17,17 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", taskController.getAllTasks);
+app.get("/add", taskController.getAddTaskForm);
+app.post("/add", taskController.addTask);
+app.get("/edit/:id", taskController.getEditTaskForm);
+app.post("/edit/:id", taskController.editTask);
+app.get("/complete/:id", taskController.completeTask);
+app.get("/uncomplete/:id", taskController.uncompleteTask);
+app.get("/delete/:id", taskController.deleteTask);
+
+app.use(errorController.error404);
 
 app.listen(port, () => {
   console.log(`La aplicación está funcionando en http://localhost:${port}`);
